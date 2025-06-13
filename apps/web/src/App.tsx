54cd8afb5cade7fb714e197cfacdef2f8108f5f1
ethankuo0218 <ethenkuo0218@gmail.com>
2025-06-13 21:07:32 +0800

    [web]: revert router change

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from './components/layout/MainLayout';
-import Home from './pages/Home';
 import { routes } from './routes';
 
 function App() {
   return (
     <Routes>
-      <Route key={'/'} path="/" element={<Home />}></Route>
+      {/* <Route key={'/'} path="/" element={<Home />}></Route> */}
 
       <Route element={<MainLayout />}>
         {routes.map(({ path, element }) => (
--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -4,6 +4,10 @@ const MarketDashboard = lazy(() => import('./pages/MarketDashboard'));
 
 export const routes = [
   {
+    path: '/',
+    element: <MarketDashboard />,
+  },
+  {
     path: '/market',
     element: <MarketDashboard />,
   },
