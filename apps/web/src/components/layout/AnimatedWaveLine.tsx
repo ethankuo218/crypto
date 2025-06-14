8d41334968ecf0a6c04ab80b96b45a0928b96dd0
ethankuo0218 <ethenkuo0218@gmail.com>
2025-06-14 19:04:28 +0800

    [web]: refactor animation line loop logic

--- apps/web/src/components/layout/AnimatedWaveLine.tsx
+++ apps/web/src/components/layout/AnimatedWaveLine.tsx
@@ -14,26 +14,40 @@ const AnimatedWaveLine: React.FC = () => {
 
     const timeline = createTimeline({
       defaults: {
-        duration: 4000,
+        duration: 2000,
         loop: true,
-        loopDelay: 250,
+        loopDelay: 0,
       },
     });
 
     timeline
-      .add(stop1.current, {
-        stopColor: [
-          { to: '#FF59F8', duration: 1000 },
-          { to: '#AE72FF', duration: 1000 },
-        ],
-      })
+      .add(
+        stop1.current,
+        {
+          stopColor: [
+            { to: '#FF59F8', duration: 200 },
+            { to: '#FFEE00', duration: 400 },
+            { to: '#5CD167', duration: 400 },
+            { to: '#00EFDF', duration: 400 },
+            { to: '#AE72FF', duration: 400 },
+            { to: '#FF59F8', duration: 200 },
+          ],
+          direction: 'normal',
+        },
+        0
+      )
       .add(
         stop2.current,
         {
           stopColor: [
-            { to: '#AE72FF', duration: 1000 },
-            { to: '#00EFDF', duration: 1000 },
+            { to: '#AE72FF', duration: 200 },
+            { to: '#FF59F8', duration: 400 },
+            { to: '#FFEE00', duration: 400 },
+            { to: '#5CD167', duration: 400 },
+            { to: '#00EFDF', duration: 400 },
+            { to: '#AE72FF', duration: 200 },
           ],
+          direction: 'normal',
         },
         0
       )
@@ -41,9 +55,14 @@ const AnimatedWaveLine: React.FC = () => {
         stop3.current,
         {
           stopColor: [
-            { to: '#00EFDF', duration: 1000 },
-            { to: '#5CD167', duration: 1000 },
+            { to: '#00EFDF', duration: 200 },
+            { to: '#AE72FF', duration: 400 },
+            { to: '#FF59F8', duration: 400 },
+            { to: '#FFEE00', duration: 400 },
+            { to: '#5CD167', duration: 400 },
+            { to: '#00EFDF', duration: 200 },
           ],
+          direction: 'normal',
         },
         0
       )
@@ -51,9 +70,14 @@ const AnimatedWaveLine: React.FC = () => {
         stop4.current,
         {
           stopColor: [
-            { to: '#5CD167', duration: 1000 },
-            { to: '#FFEE00', duration: 1000 },
+            { to: '#5CD167', duration: 200 },
+            { to: '#00EFDF', duration: 400 },
+            { to: '#AE72FF', duration: 400 },
+            { to: '#FF59F8', duration: 400 },
+            { to: '#FFEE00', duration: 400 },
+            { to: '#5CD167', duration: 200 },
           ],
+          direction: 'normal',
         },
         0
       )
@@ -61,9 +85,14 @@ const AnimatedWaveLine: React.FC = () => {
         stop5.current,
         {
           stopColor: [
-            { to: '#FFEE00', duration: 1000 },
-            { to: '#FF59F8', duration: 1000 },
+            { to: '#FFEE00', duration: 200 },
+            { to: '#5CD167', duration: 400 },
+            { to: '#00EFDF', duration: 400 },
+            { to: '#AE72FF', duration: 400 },
+            { to: '#FF59F8', duration: 400 },
+            { to: '#FFEE00', duration: 200 },
           ],
+          direction: 'normal',
         },
         0
       );
@@ -101,7 +130,7 @@ const AnimatedWaveLine: React.FC = () => {
       <path
         d="M904.5 69.5C859.917 48.9515 806.581 95.2036 739 95.7802C671.419 96.3568 655.639 26.617 602.026 30.6265C549.627 34.5452 496.222 100.269 443.446 95.7802C390.669 91.291 369.517 18.6372 273.578 53.1797C177.64 87.7222 177.15 18.6117 108.79 3.56263C40.4291 -11.4864 1 44.6596 1 44.6596"
         stroke="url(#paint0_linear_3_1702)"
-        strokeOpacity="0.5"
+        strokeOpacity="1"
         strokeWidth="2"
         fill="none"
       />
